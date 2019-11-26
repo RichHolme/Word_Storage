aa379a393e4edd4a0a006ed6a374927bbdc6466f
Richard T. Holme <Holme_Richard_T@cat.com>
2019-11-26 08:54:36 -0500

    remove api key

--- js/logic.js
+++ js/logic.js
@@ -2,13 +2,7 @@ $( document ).ready(function() {
 
 	// Your web app's Firebase configuration
 	var firebaseConfig = {
-		apiKey: "AIzaSyB_Ehzjw5lNNM36hJIwUWHowE2vxK-v7FA",
-		authDomain: "word-storage-3fdee.firebaseapp.com",
-		databaseURL: "https://word-storage-3fdee.firebaseio.com",
-		projectId: "word-storage-3fdee",
-		storageBucket: "word-storage-3fdee.appspot.com",
-		messagingSenderId: "558481923751",
-		appId: "1:558481923751:web:70cbec4544bd844b0cc582"
+		
 	};
 	// Initialize Firebase
 	firebase.initializeApp(firebaseConfig);
